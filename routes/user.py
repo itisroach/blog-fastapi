4e5059c923fc56645ef913f839e4e6f074ba028c
unknown <rouchashoori@gmail.com>
2025-06-28 00:04:32 +0330

    add user route for registeration

---
+++ routes/user.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Body
+from schema._input import UserInput
+from operations.user import UserOps
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
+from db.engine import get_db
+from fastapi import Depends
+
+
+
+router = APIRouter()
+
+@router.post("/register")
+async def register(
+    db_session: Annotated[AsyncSession, Depends(get_db)],
+    body: UserInput = Body(),
+):
+
+    user_ops = UserOps(db_session)
+
+    return await user_ops.create(body)    
