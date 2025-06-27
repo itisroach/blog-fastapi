c4f50d38c589c91c82dbfe5e97b44bb418c5b6e5
unknown <rouchashoori@gmail.com>
2025-06-27 23:22:41 +0330

    add a follder for adding input output schemas

---
+++ schema/_input.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class UserInput(BaseModel):
+
+    name: Optional[str] = "Unknown"
+
+    username: str = Field(min_length=3, max_length=32)
+    
+    password: str = Field(min_length=8, max_length=64)
