3c9f953cba5d18f67a0cde9ad603e22c51a538ed
unknown <rouchashoori@gmail.com>
2025-06-28 02:15:42 +0330

    add function for verifying (comparing) user password at login

--- utils/crypt.py
+++ utils/crypt.py
@@ -10,3 +10,12 @@ def hash_password(plain_password: str) -> str:
     hashed_pwd = bcrypt.hashpw(plain_password, salt)
 
     return hashed_pwd.decode("utf-8")
+
+
+
+def compare_password(plain: str, hashed: str) -> bool:
+
+    plain = bytes(plain, "utf-8")
+    hashed = bytes(hashed, "utf-8")
+
+    return bcrypt.checkpw(plain, hashed)
