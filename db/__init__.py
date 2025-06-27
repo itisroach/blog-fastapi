010530215930a2475018516719c523b4a8895515
unknown <rouchashoori@gmail.com>
2025-06-28 00:01:26 +0330

    add name field to database user model

--- db/__init__.py
+++ db/__init__.py
@@ -1,8 +1,8 @@
-from engine import Base
-from models import User
+from db.engine import Base
+from db.models import UserModel
 
 
 __all__ = [
     "Base",
-    "User"
+    "UserModel"
 ]
--- db/models.py
+++ db/models.py
@@ -1,13 +1,17 @@
 from db import Base
 from sqlalchemy.orm import mapped_column, Mapped
-from uuid import UUID, uuid5
+from uuid import UUID, uuid4
 
-class User(Base):
+class UserModel(Base):
 
     __tablename__ = "users"
-
-    id: Mapped[UUID] = mapped_column(primary_key=True, default_factory=uuid5)
 
     password: Mapped[str] = mapped_column()
 
     username: Mapped[str] = mapped_column(unique=True)
+
+    name: Mapped[str] = mapped_column(default="Unknown")
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, default_factory=uuid4)
+
+    
