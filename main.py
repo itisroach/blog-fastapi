c03969863987ff527671fa00a832063bc56a8199
unknown <rouchashoori@gmail.com>
2025-06-28 00:03:38 +0330

    add database connection on startup lifespan

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from routes import user
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+from db.engine import engine, Base
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
 
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 
 app.include_router(
