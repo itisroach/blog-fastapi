fdebbc102337ba4a044fd938b533f9d1e5f93fdb
unknown <rouchashoori@gmail.com>
2025-06-28 19:29:22 +0330

    create logic of expiring refresh token after one usage and deleting other after generating fresh refresh token

--- utils/jwt.py
+++ utils/jwt.py
@@ -1,41 +1,114 @@
 import jwt
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 from settings import ALGORITHM, SECRET
 from schema.output import JWTOutput
+from utils.exceptions import InvalideTokenException
+from sqlalchemy.ext.asyncio import AsyncSession
+from db.models import TokenModel
+import sqlalchemy as sqa
 
 
 class JWTHandler:
 
-    @staticmethod
-    def generate(username: str, exp_time: datetime = None) -> JWTOutput:
+    def __init__(self, db_session: AsyncSession):
+        self.db_session = db_session
+
+    async def generate(self, username: str, exp_time: datetime = None) -> JWTOutput:
+
+        token_exists = await self.check_token_existance(username)
+        
+        if token_exists:
+            await self.delete_from_db(username)
+
 
         exp_time = exp_time if exp_time else datetime.now() + timedelta(days=1)
 
-        exp_time = exp_time.timestamp()
+        exp_time = int(exp_time.timestamp())
 
         payload = {
             "exp": exp_time,
-            "username": username
+            "username": username,
+            "token_type": "access"
         }
 
         access_token = jwt.encode(payload, key=SECRET, algorithm=ALGORITHM)
 
     
-        refresh_token = JWTHandler.generate_referesh_token(username)
+        refresh_token, payload = self.generate_referesh_token(username)
         
+        await self.add_token_to_db(refresh_token, payload["exp"], payload["username"])
 
         return JWTOutput(access=access_token, refresh=refresh_token)
 
 
-    @staticmethod
-    def generate_referesh_token(username: str):
-        exp_time = datetime.now() + timedelta(days=2)
+    def generate_referesh_token(self, username: str):
+        exp_time = datetime.now() + timedelta(minutes=1)
         
         payload = {
-            "exp": exp_time.timestamp(),
-            "username": username
+            "exp": int(exp_time.timestamp()),
+            "username": username,
+            "token_type": "refresh"
         }
 
         refresh_token = jwt.encode(payload, key=SECRET, algorithm=ALGORITHM)
 
-        return refresh_token
+        return (refresh_token, payload)
+    
+
+    async def decode_and_verify_token(self, token: str) -> str:
+
+        try:
+            value = jwt.decode(token, key=SECRET, algorithms=[ALGORITHM])
+
+        except jwt.exceptions.InvalidTokenError as e:
+            
+            raise InvalideTokenException(str(e))
+        
+    
+        if value["token_type"] == "access": 
+            raise InvalideTokenException("the give token is access token, you should pass refresh token")
+        
+
+        token_exists = await self.check_token_existance(token=token)
+
+
+        if not token_exists:
+            raise InvalideTokenException("refresh token should be used once, use fresh refresh token instead")
+
+    
+        return value["username"]
+    
+
+    async def delete_from_db(self, username: str):
+
+        delete_query = sqa.delete(TokenModel).where(TokenModel.username == username)
+
+        async with self.db_session as conn:
+            await conn.execute(delete_query)
+            await conn.commit()
+
+
+    async def add_token_to_db(self, token: str, exp: int, username: str):
+
+        token_instance = TokenModel(username=username, expiration=exp, refresh_token=token)
+
+        async with self.db_session as conn:
+            conn.add(token_instance)
+            await conn.commit()
+
+
+    async def check_token_existance(self, username: str = None, token: str = None) -> bool:
+
+        query = sqa.select(TokenModel).where(sqa.or_(
+            TokenModel.username == username, 
+            TokenModel.refresh_token == token,
+        ))
+
+        async with self.db_session as conn:
+
+            result = await conn.scalar(query)
+
+            if result is None:
+                return False
+
+        return True
