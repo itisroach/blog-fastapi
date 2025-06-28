577e00bfa3f986389dc42744355bc661239d4f55
unknown <rouchashoori@gmail.com>
2025-06-28 23:47:15 +0330

    add foreign key to token table so whenever user updates their username it will also updated in token table

--- db/models.py
+++ db/models.py
@@ -1,6 +1,8 @@
 from db import Base
 from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import ForeignKey, String
 from uuid import UUID, uuid4
+
 
 class UserModel(Base):
 
@@ -11,11 +13,11 @@ class UserModel(Base):
 
     password: Mapped[str] = mapped_column()
 
-    username: Mapped[str] = mapped_column(unique=True)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
 
     name: Mapped[str] = mapped_column(default="Unknown")
 
-    id: Mapped[UUID] = mapped_column(primary_key=True, default_factory=uuid4)
+    id: Mapped[UUID] = mapped_column(primary_key=True,default_factory=uuid4)
 
     
 
@@ -26,7 +28,7 @@ class TokenModel(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, init=False)
 
-    username: Mapped[str] = mapped_column(unique=True)
+    username: Mapped[str] = mapped_column(String, ForeignKey("users.username", onupdate="CASCADE"), unique=True, nullable=False)
 
     refresh_token: Mapped[str] = mapped_column()
 
