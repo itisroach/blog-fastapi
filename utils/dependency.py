90122ce36c5bf12746f4b18207cf4b99b88d69fc
unknown <rouchashoori@gmail.com>
2025-06-28 20:37:40 +0330

    create a dependency for checking whether jwt token provided or not

---
+++ utils/dependency.py
@@ -0,0 +1,18 @@
+from fastapi.security import OAuth2PasswordBearer
+from db.engine import get_db
+from utils.jwt import JWTHandler
+from typing import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Depends
+
+
+oauth_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def jwt_required(
+    db_session = Annotated[AsyncSession, Depends(get_db)],
+    token: str = Depends(oauth_scheme)
+):
+
+    decoded_username = await JWTHandler(db_session).decode_and_verify_token(token)
+
+    return decoded_username
