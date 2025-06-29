44c16fe3301f2194c0d2b627b386ea429d464985
unknown <rouchashoori@gmail.com>
2025-06-30 00:20:38 +0330

    add class to look for any issue such as empty fields or just white space fields and etc

--- schema/_input.py
+++ schema/_input.py
@@ -1,7 +1,44 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from typing import Optional
+import re
 
-class UserInput(BaseModel):
+# a class to inherit from for checking if any field is empty or just white
+class NoWhiteSpace:
+
+    __strip_fields__: tuple[str, ...] = ()
+
+
+    @field_validator("*", mode="before")
+    def remove_white_space(cls, value, field):
+
+        if field.field_name in cls.__strip_fields__:
+
+            if not isinstance(value, str):
+                raise TypeError(f"{field.field_name} must be a string")
+            
+            if not value.strip():
+                raise ValueError(f"{field.field_name} cannot be empty or only whitespace")
+
+        return value.strip()
+    
+# a class to inherit from for checking if username contains white space in between and turn them to lowercase 
+class NormalizeUsername:
+
+    @field_validator("*", mode="before")
+    def normalize_username(cls, value, field):
+
+        if field.field_name == "username":
+            if not isinstance(value, str):
+                raise TypeError(f"{field.field_name} must be a string")
+            
+            if re.search(r'\s', value):
+                raise ValueError('username must not contain whitespace characters')
+        
+        return value.lower().strip()
+    
+
+
+class UserInput(BaseModel, NoWhiteSpace, NormalizeUsername):
 
     name: Optional[str] = "Unknown"
 
@@ -9,17 +46,24 @@ class UserInput(BaseModel):
     
     password: str = Field(min_length=8, max_length=64)
 
+    __strip_fields__ = ("name", "username", "password")
 
-class UserLoginInput(BaseModel):
+
+class UserLoginInput(BaseModel, NoWhiteSpace, NormalizeUsername):
 
     username: str
     password: str
+
+    __strip_fields__ = ("username", "password")
     
 
-class UpdateUserInput(BaseModel):
+class UpdateUserInput(BaseModel, NoWhiteSpace, NormalizeUsername):
 
     username: Optional[str] = Field(min_length=3, max_length=32, default=None)
     name: Optional[str] = Field(min_length=3, max_length=32, default=None)
+
+    __strip_fields__ = ("name", "username")
+
 
 
 class UserRefreshTokenInput(BaseModel):
@@ -27,11 +71,10 @@ class UserRefreshTokenInput(BaseModel):
     token: str
 
 
-class PostInput(BaseModel):
+class PostInput(BaseModel, NoWhiteSpace):
 
     title: str = Field(min_length=10, max_length=128)
 
     content: str = Field(min_length=128, max_length=1024)
 
-    username: str
-
+    __strip_fields__ = ("title", "content")
