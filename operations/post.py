35dc167345db25937b0cfefeed94698c3a108b70
unknown <rouchashoori@gmail.com>
2025-06-30 17:03:32 +0330

    add get operation by id in post opeartions

--- operations/post.py
+++ operations/post.py
@@ -3,6 +3,8 @@ from schema._input import PostInput
 from sqlalchemy.ext.asyncio import AsyncSession
 from db import UserModel, PostModel
 from operations.user import UserOps
+from utils.exceptions import NotFoundException
+import sqlalchemy as sqa
 
 class PostOps:
 
@@ -26,3 +28,20 @@ class PostOps:
 
 
         return PostOutput.show(post_instance, user)
+
+
+
+    async def get(self, post_id: int):
+
+        query = sqa.select(PostModel).where(PostModel.id == post_id)
+
+        async with self.db as conn:
+
+            result = await conn.scalar(query)
+
+            if result is None:
+                raise NotFoundException(PostModel.model_name_for_exceptions)
+            
+            author = await UserOps(self.db).get_by_username(result.username)
+
+            return PostOutput.show(result, author)
